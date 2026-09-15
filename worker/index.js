2a86c3f9f562ddf0bf21605d0a624d5ce8883cd6
Akki Bhat <akki@pi.co.nz>
2026-09-16 00:33:54 +1200

    Add Logitech Harmony Hub + Remote + 2x IR blasters (45 items total)

--- worker/index.js
+++ worker/index.js
@@ -68,6 +68,7 @@ const ITEM_LISTED_PRICES = {
   '2× Guitar Hero Guitars + Wireless Dongles': '$100 NZD',
   'Nikko Open-Face Helmet': '$40 NZD',
   'Squier by Fender Bullet Strat': '$150 NZD ($200 w/ cable)',
+  'Logitech Harmony Hub + Remote + 2× IR Blasters': '$55 NZD',
 };
 
 function listedPriceFor(item) {
