3d5ac8d759b9c8810192756ee5eb2a665e2ddaf5
Akki Bhat <akki@pi.co.nz>
2026-09-15 17:46:57 +1200

    Adjust draft prices: Instant Pot to $100 (retails ~$200), wands to $20

--- worker/index.js
+++ worker/index.js
@@ -52,8 +52,8 @@ const ITEM_LISTED_PRICES = {
   'PS4 DualShock 4 Controllers + Charging Dock': '$40 NZD',
   'PlayStation 3 (Slim)': '$100 NZD',
   'TCL 55C635 4K QLED Google TV': '$450 NZD',
-  'Instant Pot Duo 80 (8-Quart) 7-in-1 Multi-Cooker': '$80 NZD',
-  'Assorted Harry Potter Wands + Owl Holder': '$40 NZD',
+  'Instant Pot Duo 80 (8-Quart) 7-in-1 Multi-Cooker': '$100 NZD',
+  'Assorted Harry Potter Wands + Owl Holder': '$20 NZD',
 };
 
 function listedPriceFor(item) {
