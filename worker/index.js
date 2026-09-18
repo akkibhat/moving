05d4af48af9a121a96b54fe00d6f3bb6c4e25ab9
Akki Bhat <4131329+akkibhat@users.noreply.github.com>
2026-09-18 15:59:47 +1200

    Drop Ecosa weighted blanket price to $50

--- worker/index.js
+++ worker/index.js
@@ -126,7 +126,7 @@ const ITEM_LISTED_PRICES = {
   'Brother HL-L2400DW Mono Laser Printer': '$100 NZD',
   'Breville Hand Mixer + 2 Sets of Beaters': '$15 NZD',
   'Goldair Weighted Heated Blanket 4.5kg': '$50 NZD',
-  'Ecosa Weighted Blanket 9kg': '$100 NZD',
+  'Ecosa Weighted Blanket 9kg': '$50 NZD',
   'Microsoft Surface Laptop (1st Gen) + Dock + USB-C Adapter': '$150 NZD',
   '6-Drawer Wide Lowboy - Oak': '$80 NZD',
   '5-Drawer Tallboy - White': '$50 NZD',
